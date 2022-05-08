fe922d81008d4564000b456ea508561d11569d54
jirsh <jirsh@users.noreply.github.com>
2022-05-08 12:18:39 +0200

    Started working on adding github stats

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from "next";
+import { useEffect } from "react";
+import useSWR from "swr";
 
+const fetcher = (resource: any) => fetch(resource, {mode: "no-cors"}).then(res => res.text());
 const Home: NextPage = () => {
-  return <>Hi</>;
+  const {data} = useSWR("https://github-readme-stats.vercel.app/api?username=deluxe0x10&count_private=true&hide=stars,prs,issues,contribs&show_icons=true", fetcher);
+  console.log(data);
+  return <>{data && <div dangerouslySetInnerHTML={{__html: data}} /> }</>;
 };
 
 export default Home;
